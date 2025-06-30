9ce2ec5cd979aefcd540336ca72751bab7a509b3
boo <booblegum42@gmail.com>
2025-06-30 17:52:45 +0700

    delete/edit snippets

--- MainApp/views.py
+++ MainApp/views.py
@@ -32,7 +32,6 @@ def add_snippet_page(request):
             ...
 
 
-
 def snippets_page(request):
     snippets = Snippet.objects.all()
     context = {
@@ -49,3 +48,44 @@ def snippet_detail(request, id):
         'snippet': snippet
     }
     return render(request, 'pages/snippet_detail.html', context)
+
+
+def snippet_delete(request, id):
+    snippet = get_object_or_404(Snippet, id=id)
+    snippet.delete()
+
+    return redirect('snippets-list')
+
+
+def snippet_edit(request, id):
+    if request.method == "GET":
+        snippet = get_object_or_404(Snippet, id=id)
+        form_data = {
+            "name": snippet.name,
+            "lang": snippet.lang,
+            "code": snippet.code
+        }
+        form = SnippetForm(form_data)
+        context = {
+            "pagename": "Редактирование сниппета",
+            "form": form,
+            "edit": True,
+            "snippet": snippet
+        }
+        return render(request, 'pages/add_snippet.html', context)
+
+    if request.method == "POST":
+        snippet = get_object_or_404(Snippet, id=id)
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            lang = form.cleaned_data["lang"]
+            code = form.cleaned_data["code"]
+
+            snippet.name = name
+            snippet.lang = lang
+            snippet.code = code
+
+            snippet.save()
+            return redirect("snippets-list")
+
