2a5768723fd2b98c99252cf367cebdd49db7ffd9
boo <booblegum42@gmail.com>
2025-07-02 17:29:43 +0700

    add user to Snippet

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 LANG_CHOICES = [
     ("python", "Python"),
@@ -21,3 +22,5 @@ class Snippet(models.Model):
     code = models.TextField(max_length=5000)
     creation_date = models.DateTimeField(auto_now_add=True)
     views_count = models.PositiveIntegerField(default=0)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE,
+                             blank=True, null=True)
