f9037135a654e46aa50202ed82eba43433285ec0
boo <booblegum42@gmail.com>
2025-07-30 19:26:26 +0700

    add factories

---
+++ MainApp/test_factory.py
@@ -0,0 +1,9 @@
+import pytest
+from .factories import *
+from .models import User
+
+@pytest.mark.django_db
+def test_task1():
+    UserFactory(username="Alice")
+    user = User.objects.get(username="Alice")
+    assert user.username == "Alice"
