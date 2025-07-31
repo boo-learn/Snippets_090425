6e1e2d126a120b768aaa7d25b6ea21baff993a0e
boo <booblegum42@gmail.com>
2025-07-31 17:30:13 +0700

    add fixture factories

---
+++ MainApp/test_fixture_factories.py
@@ -0,0 +1,43 @@
+import pytest
+from .models import Tag, Snippet, Comment
+from .factories import TagFactory, SnippetFactory, CommentFactory
+
+
+# Задание-1
+@pytest.fixture
+def tag_factory():
+    def _create_tags(names):
+        tags = []
+        for name in names:
+            tags.append(TagFactory(name=name))
+        return tags
+    return _create_tags
+
+
+@pytest.mark.django_db
+def test_create_tags(tag_factory):
+    # Создаст три тега, с указанными именами
+    tags = tag_factory(names=["js", "basic", "oop"])
+
+    assert Tag.objects.count() == 3
+
+# Задание-2
+@pytest.fixture
+def snippet():
+    return SnippetFactory()
+
+
+@pytest.fixture
+def comment_factory():
+    def _create_commenys_to_snippet(snippet, n):
+        return CommentFactory.create_batch(n, snippet=snippet)
+    return _create_commenys_to_snippet
+
+
+@pytest.mark.django_db
+def test_create_comments(snippet, comment_factory):
+    comment_factory(snippet=snippet, n=6)
+
+    assert Comment.objects.count() == 6
+    for comment in Comment.objects.all():
+        assert comment.snippet == snippet
