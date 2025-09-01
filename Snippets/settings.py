926cadb536131be1f9b71347d511b31b49ed04a7
boo <booblegum42@gmail.com>
2025-09-01 18:51:48 +0700

    run in docker-container

--- Snippets/settings.py
+++ Snippets/settings.py
@@ -147,12 +147,11 @@ if os.environ.get('DATABASE_URL'):
     }
 else:
     # Разработка - SQLite
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+    DATABASES = {'default': {
+        'ENGINE': 'django.db.backends.postgresql', 'NAME': os.environ.get('DB_NAME', 'postgres'),
+        'USER': os.environ.get('DB_USER', 'postgres'), 'PASSWORD': os.environ.get('DB_PASSWORD', 'mysecretpassword'),
+        'HOST': os.environ.get('DB_HOST', 'some-postgres'), 'PORT': os.environ.get('DB_PORT', '5432'),
+    }}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
